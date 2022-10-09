45af423df1104a61a4bc539c248e3b472fd0df2e
Yuliya <juliapetrova95@gmail.com>
2022-10-09 23:28:30 +0300

    modified:   src/components/Header/index.jsx modified:   src/components/Header/index.jsx modified:   src/pages/Home.jsx modified:   src/redux/slices/posts.js

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
       <Container maxWidth="lg">
         <div className={styles.inner}>
           <Link className={styles.logo} to="/">
-            <div>MY BLOG</div>
+            <div>ARCHAKOV BLOG</div>
           </Link>
           <div className={styles.buttons}>
             {isAuth ? (
